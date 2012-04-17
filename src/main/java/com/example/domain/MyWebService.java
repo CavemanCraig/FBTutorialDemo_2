1a1d7cf09e95d74798fa2f99ce20c0c94e5d5d2c
Craig Schwarzwald <craig.schwarzwald@gmail.com>
2012-04-16 22:02:31 -0400

    Added MySQL DB and phpMyAdmin

--- src/main/java/com/example/domain/MyWebService.java
+++ src/main/java/com/example/domain/MyWebService.java
@@ -283,21 +283,31 @@ public class MyWebService {
 
 	// Performs validation on answers submitted, and adjust points.
 	@POST
-	@Path("/GameAnswers/{playerID}/{id1}/{id2}/{id3}/{name1}/{name2}/{name3}")
+	@Path("/GameAnswers/{playerID}/{id1}/{id2}/{id3}")
 	@Consumes("application/json")
 	public String postForAnswers(@PathParam("playerID") long playerID,
 			@PathParam("id1") long id1, @PathParam("id2") long id2,
-			@PathParam("id3") long id3, @PathParam("name1") String name1,
-			@PathParam("name2") String name2, @PathParam("name3") String name3) {
+			@PathParam("id3") long id3, ArrayList<String> JsonInput) {
 
 		Player player = getPlayerByFacebookID(em, playerID);
 		User user1 = getUserByFacebookID(em, id1);
 		User user2 = getUserByFacebookID(em, id2);
 		User user3 = getUserByFacebookID(em, id3);
+		String name1 = "";
+		String name2 = "";
+		String name3 = "";
 		if (null == player || null == user1 || null == user2 || null == user3) {
 			// This should never happen, but lets exit gracefully if it somehow
 			// does.
 			return "Sorry, there was an error trying to validate your answers. Please try again.";
+		}
+		if (JsonInput.size() != 3) {
+			// This needs to be the 3 names the Player is guessing
+			return "Sorry, we couldn't understand your 3 guesses.  Please try again";
+		} else {
+			name1 = JsonInput.get(0);
+			name2 = JsonInput.get(1);
+			name3 = JsonInput.get(2);
 		}
 
 		boolean correctName1 = user1.getName().equals(name1);
